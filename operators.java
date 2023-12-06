3031ad736e74b412b48e2ef35dd255b527efa806
varenyamsingh <130786405+varenyamsingh@users.noreply.github.com>
2023-12-06 22:02:18 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,19 @@
+public class operators {
+    public static void main(String[] args) {
+        //arithmetic operators
+        //binary('+', '-', '*', '/', '%')
+        //unary('++'increment, '--'decrement)
+
+        //relational operators
+        // ('==', '!=', '>', '<', '>=', '<=')
+
+        //logical operators
+        // ('&&'And, '||'Or, '!'Not)
+
+        //bitwise operators
+        // ('&'AND, '|'OR, '^'XOR, '~'one's complement, '<<'binary left shift, '>>'binary right shift)
+
+        //assignment operators
+        // ('=', '+=','-=','*=','/=','%=','<<=','>>=','&=')
+    }
+}
