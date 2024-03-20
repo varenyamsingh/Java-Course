9b5bd2bf5de93e55002c896b80dfcdc4451ace63
varenyamsingh <130786405+varenyamsingh@users.noreply.github.com>
2024-03-21 01:00:24 +0530

    Add files via upload

---
+++ patterns2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class patterns2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // int m = sc.nextInt();
+
+        // //for printing a pattern of stars
+        // for(int i=1; i<=n; i++){
+        //     for (int j=1; j<=n; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+        //for printing matrix of 1 to the input we give
+        // for(int i=1; i<=n; i++){
+        //     for (int j=1; j<=n; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+        //now printing the reverse of the numbers
+        // for(int i=1; i<=n; i++){
+        //     for (int j=1; j<=n; j++){
+        //         System.out.print(n-j+1);
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
+
+
+//for printing half pyramid of stars just change j<=m to j<=i in line 9
